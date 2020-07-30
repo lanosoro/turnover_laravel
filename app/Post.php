847f5c3ee5589cb5c1cc68d3a09b63a57d8c1582
Ianosoro <ianosori@gmail.com>
2020-07-30 16:55:49 -0700

    30th_2

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //tablename 
+    protected $table='';
+    //Primary Key
+    public $primaryKey='id';
+    //Timestamps
+    public $timestamps =true;
 }
--- database/migrations/2020_07_19_105219_create_posts_table.php
+++ database/migrations/2020_07_19_105219_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->mediumText('body1');
+            $table->mediumText('body');
             $table->timestamps();
         });
     }
