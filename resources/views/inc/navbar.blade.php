d255afa4fdd5a7e26dcfe3d09369905d4e95faa4
Ianosoro <ianosori@gmail.com>
2020-08-07 15:19:08 -0700

    name change

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-  <a class="my-0 mr-md-auto font-weight-normal" ><h5 >Tabul8git </h5></a>
+  <a class="my-0 mr-md-auto font-weight-normal" ><h5 >Tabul8 </h5></a>
   <nav class="my-2 my-md-0 mr-md-3">
     <a class="p-2 text-dark" href="/">Home</a>
     <a class="p-2 text-dark" href="/about">About us</a>
